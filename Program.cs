5c3f9834ab45a6d2222dcb1c30d29e7235582345
SanRay240 <real.sanray240@gmail.com>
2025-02-26 14:49:51 -0600

    yes

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
-﻿namespace HelloWorld_Sakiestewa_Santino
+﻿using System;
+
+namespace HelloWorld_Sakiestewa_Santino
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Asks the user for their first name and assigns it to the variable "firstName"
+            Console.Write("What is your first name? ");
+            string firstName = Console.ReadLine();
+            // Asks the user for their last name and assigns it to the variable "lastName"
+            Console.Write("What is your last name? ");
+            string lastName = Console.ReadLine();
+            // shows a greeting message with the user's first and last name
+            Console.WriteLine($"Hello," + firstName + " " + lastName + "!");
         }
-    }
-using System;
- 
-class Program
-{
-    static void Main()
-    {
-        // It will welcomes the user
-        Console.WriteLine("Welcome!");
-
-        // Asks the user for their first name and assigns it to the variable "firstName"
-        Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
-
-        // Asks the user for their last name and assigns it to the variable "lastName"
-        Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine();
-
-        // shows a greeting message with the user's first and last name
-        Console.WriteLine($"Hello, {firstName} {lastName}!");
     }
 }
