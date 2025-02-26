4623cf4a36fe37324c2216e9025bac1443ec9ff8
SanRay240 <real.sanray240@gmail.com>
2025-02-26 13:22:16 -0600

    Hell world

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld_Sakiestewa_Santino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        // It will welcomes the user
+        Console.WriteLine("Welcome!");
+
+        // Asks the user for their first name and assigns it to the variable "firstName"
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Asks the user for their last name and assigns it to the variable "lastName"
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // shows a greeting message with the user's first and last name
+        Console.WriteLine($"Hello, {firstName} {lastName}!");
+    }
+}
